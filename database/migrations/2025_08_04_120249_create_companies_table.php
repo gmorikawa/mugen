7340dbf9d57bab713b53ed4beebc29e7d6849c39
gmorikawa <morikawa.gabriel@outlook.jp>
2025-08-15 06:51:27 +0900

    Fixed company table field requirement constraint

--- database/migrations/2025_08_04_120249_create_companies_table.php
+++ database/migrations/2025_08_04_120249_create_companies_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name', 127);
-            $table->foreignUuid('country_id');
+            $table->foreignUuid('country_id')->nullable(true);
             $table->string('description', 255);
             $table->timestamps();
 
